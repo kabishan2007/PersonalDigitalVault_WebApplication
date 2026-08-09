6aab415a6387bab6501891fa258e5d472a8480d3
kabishan <kabishan353@gmail.com>
2026-08-09 18:19:40 +0530

    create models

--- PersonalDigitalVault_WebApplication/Models/User.cs
+++ PersonalDigitalVault_WebApplication/Models/User.cs
@@ -2,5 +2,15 @@
 {
     public class User
     {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } = "User";
     }
 }
